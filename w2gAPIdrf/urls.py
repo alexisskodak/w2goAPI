f8edbe68bc10f3f90a44a0cc854084aaa510e49b
alexis <skodak95alexis@gmail.com>
2020-11-10 09:42:11 +0100

    Added DetailView for GET Requsts to Gmaps API at views.py. Added simple templates for illustration purposes

--- w2gAPIdrf/urls.py
+++ w2gAPIdrf/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
+
+
+location_pattern = r'^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$'
 
 urlpatterns = [
     path('', Index.as_view(), name='api-home'),
-    path('itineraries/', ItineraryAPIView.as_view(), name='api-list'),
-    path('itinerary/<int:pk>/', ItineraryDetail.as_view(), name='api-detail')
+    path('api/itineraries/', ItineraryAPIView.as_view(), name='api-list'),
+    path('api/itinerary/<int:pk>/', ItineraryDetail.as_view(), name='api-detail'),
+    path('api/gmaps-places/', get_places_details, name='places-by-location'),
 ]
--- w2gAPIdrf/views.py
+++ w2gAPIdrf/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, generics, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .models import *
 from .serializers import *
+import googlemaps
+import environ
+import json
 
 
 class ItineraryAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -55,3 +59,42 @@ class Index(generics.GenericAPIView):
     def get(self, request):
         template_name = 'index.html'
         return render(request, template_name)
+
+
+env = environ.Env()
+environ.Env.read_env(env_file='db.env')
+API_KEY = env('API_KEY')
+gmaps = googlemaps.Client(key=API_KEY)
+
+
+def get_places_list(request):
+    location = request.GET.get('location')
+    keyword = request.GET.get('keyword')
+    radius = request.GET.get('radius')
+    places_results = gmaps.places_nearby(location=location, keyword=keyword, radius=radius)
+    places = places_results['results']
+    return places
+
+
+FIELDS = ['name', 'formatted_address', 'rating', 'type', 'icon', 'photo']
+BASE_PHOTO_URL = f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference='
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_places_details(request):
+    places_list = get_places_list(request)
+    places_details = []
+    for place in places_list:
+        place_id = place['place_id']
+        details = gmaps.place(place_id=place_id, fields=FIELDS)['result']
+        details['photos'] = details['photos'][0]['photo_reference']
+        places_details.append(details)
+
+    photo_ids = [places_details[i]['photos'] for i in range(len(places_details))]
+    photo_urls = [BASE_PHOTO_URL + f'{photo_id}&key={API_KEY}' for photo_id in photo_ids]
+
+    for i in range(len(places_details)):
+        places_details[i]['photos'] = photo_urls[i]
+    return JsonResponse(places_details, safe=False)
